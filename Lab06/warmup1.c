a03077a0e9c5130148ff6fa10f9a2a7c94eb9ebb
SorenPatel10 <m284932@usna.edu>
2026-02-19 09:37:51 -0500

    lab6p1

---
+++ Lab06/warmup1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]){
+
+    if(argc!=2){
+        fprintf(stderr, "Error: must provide an argument.\n");
+        return 1;
+    }
+
+    srand(atoi(argv[i]));
+
+    int fd[2];
+    pipe(fd);
+
+    pid_t pid = fork();
+
+    if(pid > 0){
+        close(fd[0]);
+        for(int i = 0; i < 50; i++){
+            int num = rand();
+            write(fd[1], &num, sizeof(int));
+        }
+
+        close(fd[1]);
+        wait(NULL);
+    }
+    else{
+        close(fd[1]);
+        int sum = 0;
+        int num;
+
+        while(read(fd[0], &num, sizeof(int))>0){
+            sum += num;
+        }
+        close(fd[0]);
+
+        printf("SUm: %d\n", sum);
+    }
+
+    return 0;
+}
